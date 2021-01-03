d92fbf826233c676fcdf3cc57b2ae3a6073d9854
MB <bergerm613@gmail.com>
2021-01-03 17:45:30 -0500

    refactored into converter class

--- src/main/java/coloringpage/Converter.java
+++ src/main/java/coloringpage/Converter.java
@@ -1,4 +1,39 @@
 package coloringpage;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Converter {
+    public BufferedImage toLineDrawing(File inputFile) throws IOException {
+        BufferedImage image = ImageIO.read(inputFile);
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for(int i=0; i<height; i++) {
+
+            for(int j=0; j<width; j++) {
+
+                Color c = new Color(image.getRGB(j, i));
+                int red = (int)(c.getRed() * 0.299);
+                int green = (int)(c.getGreen() * 0.587);
+                int blue = (int)(c.getBlue() *0.114);
+
+                Color newColor = new Color(red+green+blue,
+
+                        red+green+blue,red+green+blue);
+
+                image.setRGB(j,i, (255- newColor.getRGB()));
+            }
+        }
+
+        // Blur
+        // Un - invert it
+        // edge finder
+
+        return image;
+    }
 }
--- src/main/java/coloringpage/Main.java
+++ src/main/java/coloringpage/Main.java
@@ -1,4 +1,25 @@
 package coloringpage;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
+    File input = new File("/Users/esty/Downloads/taffy.jpg");
+
+    Converter converter = new Converter();
+    BufferedImage lineDrawing;
+
+    {
+        try {
+            lineDrawing = converter.toLineDrawing(input);
+
+            File output = new File("line_drawing.jpg");
+            ImageIO.write(lineDrawing, "jpg", output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
