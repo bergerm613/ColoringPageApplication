6217a0220a8b10de5aade82e1751385ff6ea7de1
MB <bergerm613@gmail.com>
2021-01-03 21:49:58 -0500

    added dodge function and did some refactoring

--- src/main/java/coloringpage/Converter.java
+++ src/main/java/coloringpage/Converter.java
@@ -2,69 +2,113 @@ package coloringpage;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
-import java.awt.image.ConvolveOp;
-import java.awt.image.Kernel;
+import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Converter {
     public BufferedImage toLineDrawing(File inputFile) throws IOException {
         BufferedImage image = ImageIO.read(inputFile);
 
+        BufferedImage grayImage = grayscaleImage(image);
+        BufferedImage invertedAndBlurredImage = invertAndBlurImage(grayImage);
+
+        return dodge(invertedAndBlurredImage, grayImage);
+    }
+
+    private BufferedImage grayscaleImage(BufferedImage image) {
+        //https://www.tutorialspoint.com/java_dip/grayscale_conversion.htm
+
         int width = image.getWidth();
         int height = image.getHeight();
 
         for(int i=0; i<height; i++) {
-
             for(int j=0; j<width; j++) {
-
                 Color c = new Color(image.getRGB(j, i));
                 int red = (int)(c.getRed() * 0.299);
                 int green = (int)(c.getGreen() * 0.587);
-                int blue = (int)(c.getBlue() *0.114);
+                int blue = (int)(c.getBlue() * 0.114);
 
-                Color newColor = new Color(red+green+blue,
+                int grayscaleColor = red + green + blue;
+                Color newColor = new Color(grayscaleColor, grayscaleColor, grayscaleColor);
 
-                        red+green+blue,red+green+blue);
-
-                image.setRGB(j,i, (255- newColor.getRGB()));
+                image.setRGB(j,i, newColor.getRGB());
             }
         }
-
-        //     Resource:   http://www.jhlabs.com/ip/blurring.html
-        float[] matrix = new float[400];
-        for (int i = 0; i < 400; i++) {
-            matrix[i] = 1.0f / 400.0f;
-        }
-
-        int blurRad = 300;
-        float[] blurKernel = new float[blurRad*blurRad];
-        for(int i =0; i<blurRad*blurRad; i++) {
-            blurKernel[i] = 1.0f/256.0f;
-        }
-
-
-        BufferedImage blurryImg = processImage(image);
-
-        return blurryImg;
+        return image;
     }
-    public  BufferedImage processImage(BufferedImage image) {
+
+    private BufferedImage invertAndBlurImage(BufferedImage image) {
+        BufferedImage newImage = cloneImage(image);
+        invertImage(newImage);
+        return blurImage(newImage);
+    }
+
+    private void invertImage(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for(int i=0; i<height; i++) {
+            for(int j=0; j<width; j++) {
+                Color c = new Color(image.getRGB(j, i));
+                int red = 255 - c.getRed();
+                int green = 255 - c.getGreen();
+                int blue = 255 - c.getBlue();
+
+                Color newColor = new Color(red, green, blue);
+                image.setRGB(j, i, newColor.getRGB());
+            }
+        }
+    }
+
+    private BufferedImage blurImage(BufferedImage image) {
         /*
         Resources:
         https://www.javatips.net/api/java.awt.image.convolveop
         https://stackoverflow.com/questions/29295929/java-blur-image
-        * */
+        */
+
         int radius = 20;
         int size = radius * 2 + 1;
         float weight = 1.0f / (size * size);
         float[] data = new float[size * size];
 
-        for (int i = 0; i < data.length; i++) {
-            data[i] = weight;
-        }
+        Arrays.fill(data, weight);
         BufferedImageOp blurFilter = new ConvolveOp(new Kernel(size, size, data), ConvolveOp.EDGE_ZERO_FILL, null);
         return blurFilter.filter(image, null);
+    }
+
+    private BufferedImage dodge(BufferedImage front, BufferedImage back) {
+        //https://www.freecodecamp.org/news/sketchify-turn-any-image-into-a-pencil-sketch-with-10-lines-of-code-cf67fa4f68ce/
+        BufferedImage result = cloneImage(front);
+
+        int width = front.getWidth();
+        int height = front.getHeight();
+
+        for(int i=0; i<height; i++) {
+            for(int j=0; j<width; j++) {
+                int frontValue = new Color(front.getRGB(j, i)).getRed(); //could be any of RGB, red is arbitrary
+                int backValue = new Color(back.getRGB(j, i)).getRed();
+
+                int newValue = frontValue * 255 / (255 - backValue);
+                if (newValue > 255 || backValue == 255) {
+                    newValue = 255;
+                }
+
+                Color newColor = new Color(newValue, newValue, newValue);
+                result.setRGB(j, i, newColor.getRGB());
+            }
+        }
+
+        return result;
+    }
+
+    private BufferedImage cloneImage(BufferedImage image) {
+        //https://bytenota.com/java-cloning-a-bufferedimage-object/
+        ColorModel colorModel = image.getColorModel();
+        WritableRaster raster = image.copyData(null);
+        boolean isAlphaPremultiplied = colorModel.isAlphaPremultiplied();
+        return new BufferedImage(colorModel, raster, isAlphaPremultiplied, null);
     }
 }
