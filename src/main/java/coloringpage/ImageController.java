c3e7e36a8a94412ac8033a560949d79a57ecb287
lliebman <lillian.liebman@gmail.com>
2021-01-05 17:39:31 -0800

    wip displaying final images

--- src/main/java/coloringpage/ImageController.java
+++ src/main/java/coloringpage/ImageController.java
@@ -3,13 +3,15 @@ package coloringpage;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 public class ImageController {
 
     private JLabel originalImageLabel;
     private JLabel finalImageLabel;
-    private Boolean isURL;
 
     public ImageController(JLabel originalImageLabel, JLabel finalImageLabel) {
         this.originalImageLabel = originalImageLabel;
@@ -18,19 +20,55 @@ public class ImageController {
 
     public void setImages(String path) {
             setOriginalImage(path);
+
+        Converter converter = new Converter();
+        BufferedImage lineDrawing;
+
+        try {
+            lineDrawing = converter.toLineDrawing(new File(path));
+            setFinalImage(lineDrawing);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public void setImages(URL url) {
         setOriginalImage(url);
 
+        Converter converter = new Converter();
+        BufferedImage lineDrawing;
+
+/*        try {
+  //          lineDrawing = converter.toLineDrawing(new File(path));
+            setFinalImage(lineDrawing);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
     }
 
+    private void setFinalImage(BufferedImage image){
+        try{
+            //Image image = ImageIO.read(file);
+            image = (BufferedImage) image.getScaledInstance(400,400, Image.SCALE_SMOOTH);
+            finalImageLabel.setIcon(new ImageIcon(image));
+        } catch (Exception e) {
+            System.out.println("Error reading Filepath");
+        }
+    }
     private void setOriginalImage(String string){
-
+        try{
+            Image image = ImageIO.read(new File(string));
+            image = image.getScaledInstance(400,400, Image.SCALE_SMOOTH);
+            originalImageLabel.setIcon(new ImageIcon(image));
+        } catch (Exception e) {
+            System.out.println("Error reading Filepath");
+        }
     }
 
     private void setOriginalImage(URL url){
-    //    URL url2 = https://assets.climatecentral.org/images/made/2020Snow-report-cover-image_1000_664_s_c1_c_c.jpg
         try{
             Image image = ImageIO.read(url);
             image = image.getScaledInstance(400,400, Image.SCALE_SMOOTH);
