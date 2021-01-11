ffdf5fcf3664a7323848220dae9313cc1d11dcfe
lliebman <lillian.liebman@gmail.com>
2021-01-10 18:58:28 -0800

    wip tests

---
+++ src/test/java/coloringpage/ConverterTest.java
@@ -0,0 +1,21 @@
+package coloringpage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void toLineDrawing() {
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+}
---
+++ src/test/java/coloringpage/ImageControllerTest.java
@@ -0,0 +1,60 @@
+package coloringpage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ImageControllerTest {
+
+    @Test
+    public void getFinalImage() {
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void setImages_withFile() {
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void testSetImages_withURL() {
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+
+    @Test
+    public void testSetImages_badLink() {
+        //given
+
+
+        //when
+
+
+        //then
+
+
+    }
+}
